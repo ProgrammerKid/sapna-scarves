7c5e5f3b2ec1b62f1c3e27f6802aa4a995477ac9
Viraj Shah <shahvirajd@gmail.com>
2016-10-26 22:13:29 -0400

    started the index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+let express = require("express");
+let app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/static"));
+
+app.get("/", routes.index);
+
+app.listen(PORT, function() {
+	console.log("Listening on port: " + PORT);
+});
